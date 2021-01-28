ba54c3f39bbb38260a11ddbff938ed57782056df
Kayvon Raphael <kayvon2@gmail.com>
2021-01-28 10:10:06 -0800

    gsed + comments + alt delete fix

--- zsh_custom/plugins/kaze_config/kaze_config.plugin.zsh
+++ zsh_custom/plugins/kaze_config/kaze_config.plugin.zsh
@@ -1,19 +1,30 @@
+### shell / terminal conveniences
+
+# cd + ls in one command
 function cdd() {
     cd "$1"
     ls
 }
 
-#do a ps aux and then grep for a process name, keeping the column names
-#args: process name
+# do a ps aux and then search for a process name (case insensitive), keeping the column names
+# brew install gnu-sed if you don't have gsed already
 psaux(){
-    ps aux | sed -n "1p; /"$1"/p;"
+    ps aux | gsed -n "1p; /"$1"/Ip;"
 }
 
+
+### git shortcuts
+
+# show git commit history in vim more where you can hit "shift + k" on the commit hash to see the contents
 alias show-me-the-money="git log  --graph --pretty=format:'%h -%d% %s (%ci - %cr) <%an>' --abbrev-commit | vi -R -c 'set filetype=git' -"
 
+# force push your branch to a remote branch
 function overwrite-branch() {
     git push origin $(git symbolic-ref --short HEAD):$1 --force
 }
+
+
+### shell history configuration
 
 HISTSIZE=10000000
 SAVEHIST=10000000
@@ -26,21 +37,28 @@ setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
 #setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
 setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
 
-# some tmux stuff
+
+### tmux configuration
+
 export TERM="screen-256color"
 alias tmux="tmux -2"
 
-## word manipulation
+
+### terminal word manipulation
+
 # set word characters so e.g. ctrl+w works like bash (back to space)
 WORDCHARS='*?_-.[]~=/&;!#$%^(){}<>:"'
 
-# set custom behavior for alt+delete bash-style to slash
+# set custom behavior for alt+delete bash-style to delete back to slash/dot/dash/underscore
 backward-kill-dir () {
     local WORDCHARS=${WORDCHARS/\/}
+    local WORDCHARS=${WORDCHARS/\.}
     local WORDCHARS=${WORDCHARS//\_\-}
     zle backward-kill-word
 }
+# bind to alt+backspace
 zle -N backward-kill-dir
 bindkey '^[^?' backward-kill-dir
 
+# ctrl+u delete from cursor to front of line
 bindkey \^U backward-kill-line
