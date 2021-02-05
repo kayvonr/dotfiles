03157fce56635a75f9dd72d32a35beff5422fd5f
Kayvon Raphael <kayvon2@gmail.com>
2021-02-05 12:49:00 -0800

    remove example zsh plugin

--- zsh_custom/plugins/kaze_config/kaze_config.plugin.zsh
+++ zsh_custom/plugins/kaze_config/kaze_config.plugin.zsh
@@ -1,3 +1,6 @@
+# Add your own custom plugins in the custom/plugins directory. Plugins placed
+# here will override ones with the same name in the main plugins directory.
+
 ### shell / terminal conveniences
 
 # cd + ls in one command
